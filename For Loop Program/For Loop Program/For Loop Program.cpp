cb35cf4681d7bb5c99465ec9458c84beb0965122
mirshahzad <meershahzad@hotmail.com>
2020-03-05 13:26:27 +0200

    For Loop Program

---
+++ For Loop Program/For Loop Program/For Loop Program.cpp
@@ -0,0 +1,20 @@
+// For Loop Program.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+using namespace std;
+
+int main()
+
+{
+	int j;
+	for (j=0; j<15; j++)
+	cout<<j*j<<""<<endl;
+	cout<<endl;
+
+	cout<<"press any key to exit";
+	getchar();
+	getchar();
+	return 0;
+}
+
