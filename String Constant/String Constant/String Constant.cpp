d1c9c7373927f7914e4a66b689f8c6e713c3101c
mirshahzad <meershahzad@hotmail.com>
2020-03-25 14:35:18 +0200

    String Constant

---
+++ String Constant/String Constant/String Constant.cpp
@@ -0,0 +1,16 @@
+// String Constant.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+using namespace std;
+int main()
+{
+	char str[]= "Farewell! thou art too dear for my possessing.:";
+	cout<<str<<endl;
+
+	cout<<"Press any key to exit...";
+	getchar();
+	getchar();
+	return 0;
+}
+
